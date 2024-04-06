805261ab36bcf1b4410df1c22fa0d7624548dfb9
sungjihyun <jhsung23@gmail.com>
2024-04-06 11:05:46 +0900

    set: router outlet

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,17 @@
 import { Navigate, RouteObject, createBrowserRouter } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import SignupPage from './pages/SignupPage';
+import App from './App';
 
 const routes: RouteObject[] = [
   {
     path: '/',
-    element: <HomePage />,
+    element: <App />,
     children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
       {
         path: '/signup',
         element: <SignupPage />,
