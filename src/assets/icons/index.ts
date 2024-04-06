50d8d1bc470b2e4c34fa2c4a8c4d26350cb33956
sungjihyun <jhsung23@gmail.com>
2024-04-06 15:15:34 +0900

    feat: Icon

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,9 @@
+import { ReactComponent as Butterfly } from './butterfly.svg';
+import { ReactComponent as LeftChevron } from './leftChevron.svg';
+import { ReactComponent as Letter } from './letter.svg';
+
+export const icons = {
+  butterfly: Butterfly,
+  leftChevron: LeftChevron,
+  letter: Letter,
+};
